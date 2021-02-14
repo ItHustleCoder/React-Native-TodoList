7a00ea19bd40b1df4f40c99d4babe3d11e4d88c1
mr_robot <mr_robot@manjaro.homenet.telecomitalia.it>
2021-02-14 19:02:47 +0100

    First settup

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Button, Alert, Text, FlatList, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: '#fff',
   },
+  content : {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
+
+const DisplayAnImage = () => {
+
+  const [todos, setTodos] = useState([
+    {text: "React Native", key: "1"},
+    {text: "VueJs", key: "2"},
+    {text: "Laravel", key: "3"},
+
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+        return  prevTodos.filter(todo => todo.key !=key);
+    });
+  };
+
+  const submitHandler = (text) => {
+
+    {/* Validation */}
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          {text: text , key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+  
+    }else {
+      Alert.alert("Oops", 'Todo deve essere maggiore di 3 caratteri', [
+        { text: "Comprende?"},
+      ]);
+    }
+
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+        Keyboard.dismiss();
+    }} >
+      <View style={styles.container}>
+        <Header />
+        {/* Content */}
+        <View style={styles.content}>
+          <AddTodo  submitHandler={submitHandler} />
+          {/* list */}
+            <View style={styles.list}>
+              <FlatList 
+                  data={todos}
+                  renderItem={({ item }) => (
+                    <TodoItem item={item} 
+                              pressHandler={pressHandler}                  
+                    />
+                  )}
+              />
+            </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+export default DisplayAnImage;
